15c1debaabca5daaeeaf3fbdc47e285dd4cfe845
Patrick Hall <pjh@umail.ucsb.edu>
2017-11-13 10:04:31 -0500

    stuff

--- BikeRide.js
+++ BikeRide.js
@@ -22,9 +22,25 @@ class Circle {
   }
 }
 
+class Bike {
+  constructor(DOM){
+    this.dom = DOM
+    this.bike = DOM.querySelector('#bike')
+  }
+
+  get style(){
+    return this.el.style
+  }
+}
+
 let svg = new Screen 
 
 let circle = new Circle(100, 100, 10)
 circle.style.fill = '#ccc;'
 
 svg.add(circle)
+
+fetch('layers/bike/bike.svg')
+  .then(r => r.text())
+  .then(xml => new DOMParser().parseFromString(xml, 'text/xml'))
+  .then(bike => window.bike = new Bike(bike))
